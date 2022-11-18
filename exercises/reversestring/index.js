1be9f5c664a8c3c2b60b6662f9733b5ec24c2df3
James Murithi <jmurithi@tccm.org>
2022-11-18 12:48:31 -0500

    Reverse strings

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // Recursive solution
+  // take last char and concat it to a
+  // call to self with new string one char less + the addition
+  // if length is zero, return an empty string
+  // if (str.length < 1) {
+  //   return '';
+  // }
+  // return str.charAt(str.length - 1) + reverse(str.slice(0, str.length - 1));
+
+  // For loop solution
+  // declare an empty string
+  // concat in reverse
+  // let reversed = '';
+  //
+  // for (let char of str)
+  //   reversed = char + reversed;
+
+  // return reversed;
+
+  return str.split('').reduce((reversed, char) => char + reversed, '');
+}
 
 module.exports = reverse;
