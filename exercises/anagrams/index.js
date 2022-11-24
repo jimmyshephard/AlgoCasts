fb32377b870f4ba8cea2d2e75e3fc1961169eadc
James Murithi <james.mwirigi@gmail.com>
2022-11-24 12:34:00 -0600

    Capitalize + Anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,28 +9,41 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  const mapA = getCharMap(stringA);
-  const mapB = getCharMap(stringB);
-
-  if (Object.keys(mapA).length !== Object.keys(mapB).length)
-    return false;
-
-  let isAnagram = true;
-
-  Object.keys(mapA).forEach(v => {
-    if (mapA[v] !== mapB[v])
-      isAnagram = false;
-  });
-
-  return isAnagram;
+  return stringA.replace(/\W/g, '')
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join('') ===
+    stringB.replace(/\W/g, '')
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join('')
 }
 
-function getCharMap(str) {
-  const map = {};
-  for (let char of str.replace(/\W/g, '').toLowerCase()) {
-    map[char] = map[char] + 1 || 1;
-  }
-  return map;
-}
+// function anagrams(stringA, stringB) {
+//   const mapA = getCharMap(stringA);
+//   const mapB = getCharMap(stringB);
+//
+//   if (Object.keys(mapA).length !== Object.keys(mapB).length)
+//     return false;
+//
+//   let isAnagram = true;
+//
+//   Object.keys(mapA).forEach(v => {
+//     if (mapA[v] !== mapB[v])
+//       isAnagram = false;
+//   });
+//
+//   return isAnagram;
+// }
+//
+// function getCharMap(str) {
+//   const map = {};
+//   for (let char of str.replace(/\W/g, '').toLowerCase()) {
+//     map[char] = map[char] + 1 || 1;
+//   }
+//   return map;
+// }
 
 module.exports = anagrams;
